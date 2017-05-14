26992248ea6a56543764977a9d4e70092d5e4a04
Tagarito <tiagomatiasvicente@gmail.com>
2017-05-14 17:43:08 +0100

    sure

--- Proj2/src/Project.lisp
+++ Proj2/src/Project.lisp
@@ -1,49 +1,79 @@
-(defmacro def-class (supers &rest slots)
+(defmacro def-class (supers &optional slots &rest amaraleautista)
   (let ((realSupers nil)
         (className nil)
         (slots_make_instance nil)
+        (vector_make_instance (vector))
+        (loop_class_slots nil)
 
 
-       )
-    (print slots)
+
+        )
+
     (if (listp supers)
       (progn  (setf className (car supers))
               (setf realSupers (cdr supers)))
       (setf className supers)
       )
-      ; (loop for element in slots
-      ;   do
-      ;   (print element)
-      ; )
-      (setf slots_make_instance (concatenate 'list (list '&key) slots))
-    `(progn
-      ;   (write-line 5))
-      (print '(defclass ,className ,realSupers ,slots))
-      (print '(defun ,(intern (concatenate 'string (string '#:MAKE-) (string className))) ,slots_make_instance
-      ;   ;  (write-line "5"))
-          (make-instance ',classname)))
-      ;;;; Define the class
-      (defclass ,className ,realSupers ,slots)
+    (print supers)
+    (print slots)
+    (print amaraleautista)
 
-      ;;;; make-className
-      (defun ,(intern (concatenate 'string (string '#:MAKE-) (string className))) ,slots_make_instance
-      ;   ;  (write-line "5"))
-          (make-instance ',classname))
-      ;; (make-instance 'b)
+    (print (not (null slots)))
+
+    (if (not (null slots))
+      (progn (setf slots_make_instance (cons '&key (cons slots amaraleautista)))
+             (setf vector_make_instance (cons 'vector (cons slots amaraleautista)))
+             (setf loop_class_slots (cons slots amaraleautista))
+      )
     )
+    (print slots_make_instance)
+    (print vector_make_instance)
+    (print loop_class_slots)
 
+
+  (print `(progn (print '(defun ,(intern (concatenate 'string (string '#:MAKE-) (string className))) ,slots_make_instance
+            ,vector_make_instance
+            ))
+            (defun ,(intern (concatenate 'string (string '#:MAKE-) (string className))) ,slots_make_instance
+                      ,vector_make_instance
+                      )
+
+            `(loop for element in ',loop_class_slots
+              do
+              (print '(defun ,(concatenate 'string (string ',className) (string '-) (string element)) ())
+                 )
+              ;(defun ,(intern (concatenate 'string (string `,className) (string '-) (string `,element))) ())
+
+              )
+
+            ;  (print (string (concatenate 'string (string ',className) (string '-) (string element))))
+            ;  (print `(defun ,(concatenate 'string (string ',className) (string '-) (string element)) ()
+            ;                   )
+            ;        )
+            ;  )
   )
-)
-
-(defun xD (supers slots)
-(let ((realSupers nil) (className nil))
-  (if (listp supers)
-    (set className (car supers))
-    (set className supers)
-    )
-)
-)
 
 
-;(setf r (make-instance 'person))
-(defclass person () ((ola :initform 0)))
+ )
+))
+
+
+
+ (setf vector_make_instance (concatenate 'list (list 'vector) slots))
+ `(progn
+
+   ;   (write-line 5))
+   ;(print '(defclass ,className ,realSupers ,slots))
+   ;(print '(defun ,(intern (concatenate 'string (string '#:MAKE-) (string className))) ,slots_make_instance
+   ;   ;  (write-line "5"))
+   ;    (make-instance ',classname)
+   ;    ,vector_make_instance)
+
+   ;)
+
+   ;;;; Define the class
+   ;(defclass ,className ,realSupers ,slots)
+   ;(print '(setf (symbol ',className) ,vector_make_instance))
+   ;(setf (symbol ',className) ,vector_make_instance)
+
+   ;;;; make-className
