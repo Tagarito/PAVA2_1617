e0793bc6a748de6b0529b9d3d6ac8b913e51e786
Tagarito <tiagomatiasvicente@gmail.com>
2017-05-18 00:08:47 +0100

    Cleaned up

--- Proj2/src/classes.lisp
+++
@@ -1,4 +0,0 @@
-(def-class animal age )
-(def-class (person animal) name )
-(def-class (besta animal) rage-lvl )
-(def-class (student besta person) course )
--- Proj2/src/o.lisp
+++
@@ -1,2 +0,0 @@
-(defun loada ()
-       (load "/home/tiagoo/FAC/PAVA/PAVA2_1617/Proj2/src/Project.lisp"))
